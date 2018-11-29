cdb3926750e37de155d46122dd20513464d8e366
Bruno Benicio <bbn_d7@hotmail.com.br>
2018-11-29 15:16:46 -0200

    fix CI

--- build/src/infra/constants/configs.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.default = {
-    KAFKA_HOST: 'localhost:9092',
-    ELASTIC_SEARCH_HOST: 'localhost:9092'
-};
